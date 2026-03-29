e3aa8bc2ddf554201b6516a5195f44d13605c252
palaksingh0392-creator <palak.singh0392@gmail.com>
2026-03-29 15:05:13 +0530

    new navmenu added

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,42 +1,34 @@
-import Image from "next/image";
-import Link from "next/link";
-
+import {
+  Menubar,
+  MenubarContent,
+  MenubarGroup,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 export default function Navbar() {
   return (
     <>
-      <nav className="bg-amber-50 p-3 ">
-        <div className="flex flex-row p-2 pl-3 ">
-          <div className="logo basis-1/3">
-            <h1 className="text-3xl text-black font-serif ml-4 font-bold">
-              Learnthat
-            </h1>
-          </div>
-          <div className="links basis-2/3">
-            <ul className="text-black flex flex-row gap-12 pt-2 font-sans text-l font-medium ">
-              <li>
-                <Link href="#">Home</Link>
-              </li>
-              <li>
-                <Link href="#">Courses</Link>
-              </li>
-              <li>
-                <Link href="#">Tutorials</Link>
-              </li>
-              <li>
-                <Link href="#">Blogs</Link>
-              </li>
-              <li>
-                <Link href="#">Notes</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="links pt-1 basis-3/3">
-            <button className=" text-l w-22  rounded-2xl h-8 text-black border">
-              Contact
-            </button>
-          </div>
-        </div>
-      </nav>
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarContent>
+            <MenubarGroup>
+              <MenubarItem>
+                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+              </MenubarItem>
+              <MenubarItem>New Window</MenubarItem>
+            </MenubarGroup>
+            <MenubarSeparator />
+            <MenubarGroup>
+              <MenubarItem>Share</MenubarItem>
+              <MenubarItem>Print</MenubarItem>
+            </MenubarGroup>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
     </>
   );
 }
