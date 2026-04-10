6d268bfb595085f23cb496b7cc770f6084e94b95
palaksingh0392-creator <palak.singh0392@gmail.com>
2026-04-10 16:31:22 +0530

    course page altered

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -5,11 +5,11 @@ export default function CoursesPage() {
   return (
     <div className="container mx-auto py-12">
       <div className="text-center max-w-2xl mx-auto mb-12">
-        <p className="text-sm text-muted-foreground uppercase">
+        <p className="text-sm text-muted-foreground uppercase pt-18">
           Explore our collection
         </p>
 
-        <h1 className="text-4xl font-bold mt-2">Popular Courses</h1>
+        <h1 className="text-6xl font-bold mt-2">Popular Courses</h1>
 
         <p className="text-muted-foreground mt-3">
           Dive into curated courses designed to help you master new skills.
